7b384b62cf9c6633d31c8160b9ac1d52eaf5fb4f
Tharindu Bandara <tharindu.bandara@madmobile.com>
2024-07-31 06:17:06 +0530

    backend.yml

--- backend/src/test/java/com/example/backend/Lab12BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/Lab12BackendApplicationTests.java
@@ -1,13 +1,31 @@
+// package com.example.backend;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class Lab12BackendApplicationTests {
+
+//     @Test
+//     void contextLoads() {
+//     }
+
+// }
+
+
 package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestConfiguration.class)
 class Lab12BackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
